d9020847f057e666f455e8cd83857454a6ec0bf9
wonbeanie <goobb2580@gmail.com>
2026-02-24 00:57:35 +0900

    feat: 현재 보유 종목 파이차트 추가
    - echarts-for-react 라이브러리를 사용하여 파이 차트를 구현하였습니다.

--- app/components/AnalyticsView.tsx
+++ app/components/AnalyticsView.tsx
@@ -1,11 +1,12 @@
 import { Card, Chip, Grid, Typography } from '@mui/material'
-import React from 'react'
+import CurrentStocksPieChart from './CurrentStocksPieChart'
 
 export default function AnalyticsView() {
+
   return (
     <div className="animate-in fade-in zoom-in-95 duration-500">
       <Grid container spacing={3}>
-        <Grid size={{ xs: 12, lg: 8 }}>
+        <Grid size={{ xs: 12, lg: 7 }}>
           <Card className="shadow-xl border-none rounded-3xl p-6 bg-white">
             <div className="flex justify-between items-center mb-6">
               <Typography className="font-black text-gray-800">자산 수익률 추이</Typography>
@@ -16,13 +17,8 @@ export default function AnalyticsView() {
             </div>
           </Card>
         </Grid>
-        <Grid size={{ xs: 12, lg: 4 }}>
-          <Card className="shadow-xl border-none rounded-3xl p-6 bg-white">
-            <Typography className="font-black text-gray-800 mb-6">포트폴리오 비중</Typography>
-            <div className="h-[400px] w-full bg-gray-50 rounded-2xl border border-dashed border-gray-200 flex items-center justify-center">
-              <Typography className="text-gray-300 font-medium italic">Pie Chart Area</Typography>
-            </div>
-          </Card>
+        <Grid size={{ xs: 12, lg: 5 }}>
+          <CurrentStocksPieChart />
         </Grid>
       </Grid>
     </div>
---
+++ app/components/CurrentStocksPieChart.tsx
@@ -0,0 +1,58 @@
+import ReactECharts from 'echarts-for-react';
+import { useAtomValue } from 'jotai';
+import { stockDashboardAtom } from '../store/atoms';
+import { useEffect, useState } from 'react';
+import { Card, Typography } from '@mui/material';
+
+export default function CurrentStocksPieChart() {
+  const {currentStocks} = useAtomValue(stockDashboardAtom);
+  const [currentStocksOption, setCurrentStocksOption] = useState({});
+
+  useEffect(()=>{
+    initPieChat();
+  },[]);
+
+  const initPieChat = () => {
+    const pieData = Object.entries(currentStocks).map(([stockName, stock]) => {
+      return {
+        name: stockName,
+        value: stock.amountInput
+      }
+    }).sort((a, b) => b.value - a.value);
+
+    setCurrentStocksOption({
+      tooltip: {
+        trigger: 'item',
+        formatter: '{b}({d}%)'
+      },
+      series: [
+        {
+          name: '자산 비중',
+          type: 'pie',
+          radius: '60%', // 차트 크기
+          center: ['50%', '50%'], // 차트 중심 위치
+          data: pieData,
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    })
+  }
+
+  return (
+    <Card className="shadow-xl border-none rounded-3xl p-6 bg-white">
+      <Typography className="font-black text-gray-800 mb-6">현재 보유 종목 비중</Typography>
+      <div className="h-[400px] w-full bg-gray-50 rounded-2xl border border-dashed border-gray-200 flex items-center justify-center">
+        <ReactECharts 
+          option={currentStocksOption} 
+          style={{ height: '100%', width: '100%' }} 
+        />
+      </div>
+    </Card>
+  )
+}
