5f56a01731e7ec676282d4f6418709b0c00e97fe
wonbeanie <goobb2580@gmail.com>
2026-02-25 21:51:14 +0900

    chore(footer): github 푸터 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "../components/setup/Providers";
 import GlobalSetup from "../components/setup/GlobalSetup";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
             {children}
           </GlobalSetup>
         </Providers>
+        <Footer />
       </body>
     </html>
   );
